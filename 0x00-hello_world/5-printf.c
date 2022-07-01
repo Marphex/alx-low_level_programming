1a4069d9fb6cd67c22f57856fb648bc5cedee232
Marphex <maruffs@gmail.com>
2022-07-01 07:22:26 -0700

    0-preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+int main (void)
+{
+	char c;
+	int i;
+	long int long i;
+	long long int lx2_i;
+	float f;
+	printf("size of char: %lu byte(s) \n", (unsigned long) size of (c));
+	printf("size of char: %lu byte(s) \n", (unsigned long) size of (c));
+	printf("size of char: %lu byte(s) \n", (unsigned long) size of (c));
+	printf("size of char: %lu byte(s) \n", (unsigned long) size of (c));
+	printf("size of char: %lu byte(s) \n", (unsigned long) size of (c));
+
