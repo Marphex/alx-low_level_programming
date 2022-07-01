01faf678a6a66fd9fdd883bc2a5aa2724d970953
Marphex <maruffs@gmail.com>
2022-07-01 06:51:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-include <stdio.h>
-/**
-*main - entry point
-*
-*Return: 0 after printing the function
-*/
+#!/bin/bash
+#include <stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
 }
